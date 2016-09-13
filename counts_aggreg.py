ff2264c614ccdbd6400334370d47479ed1602658
Alex Fok <alexfokil@gmail.com>
2016-09-13 19:43:48 +0300

    Create counts_aggreg.py

---
+++ counts_aggreg.py
@@ -0,0 +1,70 @@
+import os
+import shutil
+import sys
+from pyspark import SparkContext, SparkConf
+
+###########################################################
+################
+## Helpers
+# Converts RDD element into blank separated line to be written to file
+def toCSVLine(data):
+  return ' '.join(str(d) for d in data)
+
+def touchFile(filename):
+  try:
+    os.utime(filename, None)
+  except:
+    open(filename, 'a').close()    
+
+def silentRemove(filename):
+  try:
+    if os.path.exists(filename):
+	  if os.path.isfile(filename):
+        os.remove(filename)
+      else:
+        shutil.rmtree(filename)
+  except:
+    print("Failed to delete file, trying to continue execution")
+
+## Main flow
+if __name__ == "__main__":
+################
+## Main
+
+	if len(sys.argv) != 2:
+	  print("Usage: counts_aggreg input_file")
+	  exit(-1)
+	infile=sys.argv[1]
+
+	# create Spark context with Spark configuration
+	conf = SparkConf().setAppName("Counts Aggregations")
+	sc = SparkContext(conf=conf)
+
+	## Calculate new_counters from file sys.argv[1]
+	new_counters = sc.textFile(infile).map(lambda x: (x.split(" ")[0], int(x.split(" ")[1]))).reduceByKey(lambda a, b: a + b)
+	print new_counters.collect()
+
+	################
+	## touch sum.txt
+	touchFile("sum.txt")
+	
+	## Load old_counters from file "sum.txt"
+	old_counters = sc.textFile("sum.txt").map(lambda x: (x.split(" ")[0], int(x.split(" ")[1]))).reduceByKey(lambda a, b: a + b)
+	print old_counters.collect()
+
+	################
+	## Aggregate new_counters with old_counters - in agg_counts
+	agg_counts = old_counters.union(new_counters).reduceByKey(lambda a, b: a + b)
+	# agg_counts = agg_counts.union(new_counters).reduceByKey(lambda a, b: a + b)
+	## Cache agg_counts
+	agg_counts.persist()
+	## print agg_counts
+	print agg_counts.collect()
+
+	# Delete old "sum.txt" file
+	silentRemove("sum.txt")
+
+	# Save agg_counts back to "sum.txt"
+	agg_counts.coalesce(1).map(toCSVLine).saveAsTextFile("sum.txt")
+
+#########################################################
